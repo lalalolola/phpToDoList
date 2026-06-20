75f11a7312b200810f247286f3b0400fd5864321
lalalolola <milenaallayarova.02@mail.ru>
2026-06-21 01:44:29 +0300

    Небольшая правка

--- index.php
+++ index.php
@@ -4,13 +4,12 @@ require_once('functions.php');
 require_once('data.php');
 
 $page_content = include_template('main.php', [
-    'tasks' => $tasks
+    'tasks' => $tasks,
+    'projects' => $projects
 ]);
 
 $layout_content = include_template('layout.php', [
     'content' => $page_content,
-    'projects' => $projects,
-    'tasks' => $tasks,
     'title' => 'Дела в порядке'
 ]);
 
--- templates/layout.php
+++ templates/layout.php
@@ -33,27 +33,7 @@
         </header>
 
         <div class="content">
-            <section class="content__side">
-                <h2 class="content__side-heading">Проекты</h2>
-
-                <nav class="main-navigation">
-                    <ul class="main-navigation__list">
-                        <?php foreach($projects as $project): ?>
-                        <li class="main-navigation__list-item">
-                            <a class="main-navigation__list-item-link" href="#"><?= htmlspecialchars($project); ?></a>
-                            <span class="main-navigation__list-item-count"><?= countProjectElements($tasks, $project); ?></span>
-                        </li>
-                        <?php endforeach; ?>
-                    </ul>
-                </nav>
-
-                <a class="button button--transparent button--plus content__side-button"
-                   href="pages/form-project.html" target="project_add">Добавить проект</a>
-            </section>
-
-            <main class="content__main">
-              <?= $content; ?>
-            </main>
+            <?= $content; ?>
         </div>
     </div>
 </div>
